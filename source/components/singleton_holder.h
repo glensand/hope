8cf1e441fb6f57f67d93c31fef528b65214f56d4
glensand <bezborodoff.gleb@gmail.com>
2021-03-03 21:36:41 +0700

    deleted singleton holder

--- source/components/singleton_holder.h
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) 2020 - 2021 Gleb Bezborodov - All Rights Reserved
- * You may use, distribute and modify this code under the
- * terms of the MIT license.
- *
- * You should have received a copy of the MIT license with
- * this file. If not, please write to: bezborodoff.gleb@gmail.com, or visit : https://github.com/glensand/hope
- */
-
-#pragma once
